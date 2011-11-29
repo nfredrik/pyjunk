afc16b6b6294105ac22475717a65dbb3da8fe671
Fredrik Sv?rd <fredrik.svard@titv.se>
2011-11-29 22:55:04 +0100

    cosmetic changes

--- pings/ping.py
+++ pings/ping.py
@@ -4,28 +4,30 @@ import datetime
 
 #
 #PING sun.com (137.254.16.113) 56(84) bytes of data.
-# 64 bytes from bigip-www-legacy-sun-cms-adc.oracle.com (137.254.16.113): icmp_seq=1 ttl=235 time=157 ms
+# 64 bytes from bigip-www-legacy-sun-cms-adc.oracle.com (137.254.16.113):
+# icmp_seq=1 ttl=235 time=157 ms
 #
-#
+# TODO: implement different wayt to retrieve time
+
+
 class Ping(object):
 
     def __init__(self, dest):
         self.dest = dest
-#        self.ptime = time.ctime()
         self.now = datetime.datetime.today()
         self.ptime = self.now.strftime('%Y-%m-%d %H:%M:%S')
         cmd = Command('ping -c 1 ' + dest)
+
         if cmd.get_status():
             print 'Failed to ping:', dest
             self.ttl = '0' 
             self.response = '0.0'
             return
 
-#        print cmd.get_string_output()
-        self.m = re.search(r"ttl=(\d*) time=(\d*.\d*)", cmd.get_string_output())
-#        print self.m.group(0)
-        self.ttl = self.m.group(1)
-        self.response = self.m.group(2)
+        self.tnt = re.search(r"ttl=(\d*) time=(\d*.\d*)", cmd.get_string_output())
+        self.ttl = self.tnt.group(1)
+        self.response = self.tnt.group(2)
+
     def get_ttl(self):
         return self.ttl
 

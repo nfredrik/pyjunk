4831538fb959c31f2f97f67ee0b1cd2c702a8c02
Fredrik Sv?rd <fredrik.svard@titv.se>
2011-12-18 16:13:49 +0100

    Added script stuff

---
+++ bash_scripts/pipes_ands.sh
@@ -0,0 +1,17 @@
+#/usr/bin/sh 
+
+#set -x
+
+name=kalle
+name1="kalle kula"
+name2="$name kula"
+
+echo han heter $name2
+
+
+## second statement executes IF first statement went well (true)
+(ls | grep pipes_ands.sh > /dev/null) && echo $0 finns 
+
+
+## second statement executes IF first statement went wrong (false)
+(ls | grep nisse > /dev/null) || echo nisse finns inte 

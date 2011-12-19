1fa96fb36e7413b2bbfcb6045403455c345c1404
fredrik <fredrik@linux-f1uk.site>
2011-12-19 23:20:25 +0100

    small

--- subversion/post-commit.py
+++ subversion/post-commit.py
@@ -17,13 +17,13 @@ def main(repopath, rev):
 
     # Check if attempt to commit in tag
     log_msg = commands.getoutput(SVNLOOK +' changed -r '+ rev + ' ' + repopath)
-    if (re.match('^A\W.*tags\/.*$', log_msg.rstrip()) != None):
+#    sys.stderr.write('nisse pisse:' + log_msg)
+#    sys.exit(1)
 
+    if (re.match('^A\W.*tags\/.*', log_msg.rstrip()) != None):
         sys.stderr.write('Created new tag:' + repopath + ' revision' + rev)
         sys.exit(1)           
 
-    sys.stderr.write(log_msg)
-    sys.exit(1)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:

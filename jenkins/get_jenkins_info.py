ae6a7560afac685acdf4504577d1d231ec993069
fredrik <fredrik@linux-f1uk.site>
2012-02-07 02:21:59 +0100

    new stuff

--- jenkins/get_jenkins_info.py
+++ jenkins/get_jenkins_info.py
@@ -1,5 +1,20 @@
 import urllib
+import sys
  
-obj = eval(urllib.urlopen("http://byggserver02/api/python").read());
+#obj = eval(urllib.urlopen("http://127.0.0.1:8080/api/python").read());
+
+#obj = eval(urllib.urlopen("http://127.0.0.1:8080/job/remote-behor/8/console/api/python").read());
+
+# obj = eval(urllib.urlopen("http://127.0.0.1:8080/job/remote-behor/8/console/api/python").read());
+
+obj = urllib.urlopen("http://127.0.0.1:8080/job/remote-behor/8/console/")
+
+nisse = obj.read()
+
+# print obj
+
+sys.exit(2)
+
 for job in obj['jobs']:
+   print job['url']
    print job['name']

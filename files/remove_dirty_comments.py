fe38c6e8cb08cd2448277b621f3ce403cd5b376d
Fredrik Sv?rd <fredrik.svard@titv.se>
2012-03-14 22:55:03 +0100

    updated dirty comments

--- files/remove_dirty_comments.py
+++ files/remove_dirty_comments.py
@@ -1,16 +1,41 @@
 import re
+from tempfile import mkstemp
+from shutil import move
+import os
 
-def somepredicate(line):
+def row_2_remove(line):
     if re.match('^(HPC[A-Z\d]{3}\*.*)', line):
         return True
     return False
 
-readfile = open('PAOB.pco', 'r')
-writefile = open('PAOB.ng', 'w')
 
+pco_file = 'PAOB.pco'
+
+#Create temp file
+fh, abs_path = mkstemp()
+
+
+writefile = open(abs_path,'w')
+readfile = open(pco_file, 'r')
+remove_cntr = 0
+
+# Check rows with non relevant comments
 for line in readfile:
-  if not somepredicate(line):
+  if not row_2_remove(line):
     writefile.write(line)
+  else:
+    remove_cntr +=1  
+
+#Close temp file
+writefile.close()
+os.close(fh)
 
 readfile.close()
-writefile.close()
+
+#Remove original file
+os.remove(pco_file)
+
+#Move new file
+move(abs_path, pco_file)
+
+print 'Number of rows removed:', remove_cntr

adaab4671073ba09dac69ea0816f7c8cdae21487
Fredrik Sv?rd <fredrik.svard@titv.se>
2012-03-20 21:31:41 +0100

    singleton

---
+++ DesignPattern/singleton.py
@@ -0,0 +1,26 @@
+
+class Singleton(object):
+    __single = None
+    def __init__( self ):
+        if Singleton.__single:
+            raise Singleton.__single
+        Singleton.__single = self    
+
+
+class Child( Singleton ):
+    def __init__( self, name ):
+        Singleton.__init__( self )
+        self.__name = name
+    def name( self ):
+        return self.__name
+
+class Junior( Singleton ):
+    pass 
+
+
+def Handle( x = Singleton ):
+    try:
+        single = x()
+    except Singleton, s:
+        single = s
+    return single 

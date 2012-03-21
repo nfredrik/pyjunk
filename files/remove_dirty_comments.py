fbfdd8b6f442d010045f0a5ebc011b0510de2b33
fsv <fsv@byggserver02.bolagsverket.se>
2012-03-21 14:36:41 +0100

    dirty comments

--- files/remove_dirty_comments.py
+++ files/remove_dirty_comments.py
@@ -2,40 +2,54 @@ import re
 from tempfile import mkstemp
 from shutil import move
 import os
+import sys
 
 def row_2_remove(line):
     if re.match('^(HPC[A-Z\d]{3}\*.*)', line):
         return True
     return False
 
-
-pco_file = 'PAOB.pco'
-
-#Create temp file
-fh, abs_path = mkstemp()
+def main(): 
 
 
-writefile = open(abs_path,'w')
-readfile = open(pco_file, 'r')
-remove_cntr = 0
+    args = sys.argv[1:]
+#    pco_file = 'PAOB.pco'
+    pco_file = args[0]
 
-# Check rows with non relevant comments
-for line in readfile:
-  if not row_2_remove(line):
-    writefile.write(line)
-  else:
-    remove_cntr +=1  
+    if not args:
+      print 'usage:<file to convert>'
+      sys.exit(1)
 
-#Close temp file
-writefile.close()
-os.close(fh)
+    print args
 
-readfile.close()
+    #Create temp file
+    fh, abs_path = mkstemp()
 
-#Remove original file
-os.remove(pco_file)
+    writefile = open(abs_path,'w')
+    readfile = open(pco_file, 'r')
+    remove_cntr = 0
 
-#Move new file
-move(abs_path, pco_file)
+    # Check rows with non relevant comments
+    for line in readfile:
+      if not row_2_remove(line):
+        writefile.write(line)
+      else:
+        remove_cntr +=1  
 
-print 'Number of rows removed:', remove_cntr
+    #Close temp file
+    writefile.close()
+    os.close(fh)
+
+    readfile.close()
+
+    #Remove original file
+    os.remove(pco_file)
+
+    #Move new file
+    move(abs_path, pco_file)
+
+    print 'Number of rows removed:', remove_cntr
+
+
+if __name__ == '__main__':
+  main()

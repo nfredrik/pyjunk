0a7e251869f39ecde3ea8f8ec382f786a8f554fc
fsv <fsv@byggserver02.bolagsverket.se>
2012-03-30 11:20:52 +0200

    commit reluctant

--- subversion/pre-commit.py
+++ subversion/pre-commit.py
@@ -2,35 +2,42 @@
 """
 Subversion pre-commit hook which currently checks that:
 
-* Make it impossible to commit to a tag, that for some reason always be read-only
+* Only cm can commit at this stage
+
+* Make it impossible to commit to a tag, since it should be regarded as read-only
 
 * The commit contains a commit message to avoid commiting empty changesets which 
   tortoisesvn seems to have a habbit of committing.
-
-Based on http://svn.collab.net/repos/svn/branches/1.2.x/contrib/hook-scripts/commit-block-joke.py
-and hooks/pre-commit.tmpl
-
-Hacked together by Jacques Marneweck <jacques@php.net>
 """
 
+import os
 import re
 import sys
 import commands
 
+LEN_OF_COMMIT_MSG = 5
 SVNLOOK='/usr/bin/svnlook'
+cmguys =  ['cm', 'jje', 'olk', 'fsv']
 
 def main(repos, txn):
 
-    # Check if attempt to commit in tag
     log_msg = commands.getoutput(SVNLOOK +' changed -t '+ txn + ' ' + repos)
+
+    # Check that it's a cm guy that's committing!
+    author = commands.getoutput(SVNLOOK + ' author ' + repos)
+
+    if  author.rstrip() not in cmguys and re.match('^.*trunk\/.*$', log_msg.rstrip()) != None:
+        sys.stderr.write ("Sorry, only cm guys allowed to commit to trunk at this phase of the project!\n")
+        sys.exit(1)
+
+    # Check if attempt to commit in tag
     if (re.match('^U\W.*tags\/.*$', log_msg.rstrip()) != None):
         sys.stderr.write ("Sorry, tags read-only, not allowed to commit!\n")
         sys.exit(1)
 
     # Check if valid commit message  
-    log_msg = commands.getoutput(SVNLOOK +' log -t '+ txn + ' ' + repos).rstrip()
 
-    if len(log_msg) < 10:
+    if len(log_msg) < LEN_OF_COMMIT_MSG:
         sys.stderr.write ("Please enter a commit message which details what has changed during this commit.\n")
         sys.exit(1)
 
@@ -41,3 +48,5 @@ if __name__ == '__main__':
         sys.stderr.write("Usage: %s REPOS TXN\n" % (sys.argv[0]))
     else:
         main(sys.argv[1], sys.argv[2])
+
+

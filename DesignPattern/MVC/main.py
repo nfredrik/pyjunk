0a77f3f6b5d358e095ff43a2b8ac0aad3bc3396d
Fredrik Sv?rd <fredrik.svard@titv.se>
2012-04-10 09:04:21 +0200

    corrections of mvc

--- DesignPattern/MVC/main.py
+++ DesignPattern/MVC/main.py
@@ -2,7 +2,7 @@ import mvc
 controller = mvc.Controller()
  
 # Displaying Summary for defect id # 2
-print controller.getDefectSummary(2)
+controller.getDefectSummary(2)
  
 # Displaying defect list for 'ABC' Component
-print controller.getDefectList('ABC')
+controller.getDefectList('ABC')

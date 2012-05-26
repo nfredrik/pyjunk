6f2e957151652b8fe7b42475ee090145e092f2f4
nfredrik <nfredrik@localhost>
2012-05-26 15:25:30 +0200

    mer robust

--- files/rmdirectories.py
+++ files/rmdirectories.py
@@ -2,12 +2,14 @@ import os
 import sys
 import shutil
 import logging
+import logging.handlers
 from filestatus import FileStatus
 from operator import itemgetter
 
 OK,ERROR = 0, 1
 DIRECTORYS = '.'
 SAVEDAYS= 2
+LOG_FILENAME= 'loggfiler.log'
 
 def enumeratedir(path):
     """Returns all the directories in a directory as a list"""
@@ -30,6 +32,13 @@ def main(args):
     # Create logger instance
     logger = logging.getLogger('removing directories')
     
+    # Add the log message handler to the logger
+    #handler = logging.handlers.RotatingFileHandler(LOG_FILENAME,
+    #                                           maxBytes=20000,
+    #                                           backupCount=5,
+    #                                           )
+    #logger.addHandler(handler)
+    
     # Get the directories that need to be checked
     logger.debug('Get directories')
     dirs = enumeratedir(DIRECTORYS)
@@ -48,7 +57,7 @@ def main(args):
     if dir_list[:len(dir_list)-SAVEDAYS] and len(dir_list) > SAVEDAYS:
         for dir in dir_list[:len(dir_list)-SAVEDAYS]:
             logger.debug('Want to delete: %s', dir[0])
-            shutil.rmtree(dir[0])
+            shutil.rmtree(dir[0],ignore_errors=True)
     else: 
         logger.debug('Nothing to delete')
  

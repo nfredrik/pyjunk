e1c982ecb4b7fe5dc8714a629070c177f46db447
fsv <fsv@byggserver02.bolagsverket.se>
2012-11-06 13:49:35 +0100

    test

--- bash_scripts/sed_ws.sh
+++ bash_scripts/sed_ws.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 set -x
 
 export WORKSPACE=$1
@@ -9,4 +10,7 @@ cd  $WORKSPACE/src
 cp antcobolBuild antcobolBuildtest
 
 echo 'WORKSPACE: ' $WORKSPACE
+
+#sed -i "s/\.[0-9A-Za-z\/]*\(\/src[0-9A-Za-z/\.]*pco\)/\.${WORKSPACE}\1/"  ./antcobolBuildtest
+#sed -i 's/\.[0-9A-Za-z\/]*\(\/src[0-9A-Za-z/\.]*pco\)/\.$WORKSPACE\1/'  ./antcobolBuildtest
 sed -i "s#\.[0-9A-Za-z\/]*\(\/src[0-9A-Za-z/\.]*pco\)#\.${WORKSPACE}\1#"  ./antcobolBuildtest

2a0a805eaf8aeda0371ad87e500227c67b36e0a2
fsv <fsv@byggserver02.bolagsverket.se>
2012-12-13 14:54:26 +0100

    added exceptions examples

---
+++ exceptions/exceptions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+class ExampleErr(Exception): pass
+
+
+class Example(object):
+    def __init__(self):
+        raise ExampleErr('wrong')
+
+
+try:
+    example = Example()
+    print 'Hej' 
+    raise Exception('Aj')
+
+except:
+    (exc_class, exc_object, exc_traceback) = sys.exc_info()
+    print"""internal and completely unexpected problem,
+    manifested as %s""" % str(exc_class)
+

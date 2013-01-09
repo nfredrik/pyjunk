6fc0072079a65bcf1289aaffd71c60847e011ad0
fsv <fsv@byggserver02.bolagsverket.se>
2013-01-09 13:50:27 +0100

    class methods

---
+++ class_methods/class_methods.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+class ClassMethods(object):
+
+    def __new__(self):
+        pass
+    def __init__(self,args):
+        pass
+    def __del__(self):
+        pass
+    def __repr__(self):
+        pass
+    def __str__(self):
+        pass
+    def __cmp__(self,other):
+        pass
+    def __index__(self):
+        pass
+    def __hash__(self):
+        pass
+    def __getattr__(self,name):
+        pass
+    def __getattribute__(self):
+        pass
+    def __setattr__(self,name,attr):
+        pass
+    def __delattr__(self,name):
+        pass
+    def __call__(self,args, kwargs):
+        pass
+    def __lt__(self,other):
+        pass
+    def __le__(self,other):
+        pass
+    def __lt__(self,other):
+        pass
+    def __gt__(self,other):
+        pass
+    def __ge__(self,other):
+        pass
+    def __eq__(self,other):
+        pass
+    def __ne__(self,other):
+        pass
+    def __nonzero__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+

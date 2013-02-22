b907a8ce7b8c878165a539eea84a64fe82312e09
fsv <fsv@byggserver02.bolagsverket.se>
2013-02-22 17:40:29 +0100

    more good stuff

--- bash_scripts/assert.sh
+++ bash_scripts/assert.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# assert.sh
 #set -x
+
+#--------------------------------------------------------------
+function debug 
+{
+    if [ $debug ]; then
+	echo -e $1
+    fi
+}
 
 #--------------------------------------------------------------
 function assert()
 {   
 
-    # NDEBUG set do not care about assert
+    # If NDEBUG set do not care about assert
     if [ -n "$NDEBUG" ]
     then
-         echo 'NBDEBUG:' $NDEBUG
         return
     fi
 
@@ -29,72 +35,14 @@ function assert()
     fi  
 }
 
-#--------------------------------------------------------------
-function oldassert ()                 #  If condition false,
-{                         #+ exit from script
-                          #+ with appropriate error message.
-  E_PARAM_ERR=98
-  E_ASSERT_FAILED=99
-
-
-  if [ -z "$2" ]          #  Not enough parameters passed
-  then                    #+ to assert() function.
-    return $E_PARAM_ERR   #  No damage done.
-  fi
-
-  lineno=$2
-#  echo "---- 1 ar:", $1
-  if [ ! $1 ] 
-#  if [[ ! -n $1 || ! $1 ]] 
-  then
-    echo "Assertion failed:  \"$1\""
-#    echo "File \"$0\", line $lineno"    # Give name of file and line number.
-    echo 'lineno, function, file:' `caller 0`
-    exit $E_ASSERT_FAILED
-  # else
-  #   return
-  #   and continue executing the script.
-  fi  
-} # Insert a similar assert() function into a script you need to debug.    
-#--------------------------------------------------------------
-
-#EXITCODE_ZERO='\$\? -eq 0'
-
-#EXITCODE_ZERO=`echo -e ${?} -eq 0`
-
-#EXITCODE_ZERO=$(cat <<EOF 
-#${?} -eq 0
-#EOF
-#)
-
-#echo 'har har du:' $EXITCODE_ZERO ':'
 
 cat nisse &>/dev/null
 
-#NDEBUG=true
+NDEBUG=true
 
-assert "${?} -eq 0"
-#assert  $EXITCODE_ZERO $LINENO
+#assert "${?} -eq 0"
 
-a=3
+a=8
 b=4
-condition="$a -lt $b"     #  Error message and exit from script.
-                          #  Try setting "condition" to something else
-                          #+ and see what happens.
-
-# assert "$condition" $LINENO
-# The remainder of the script executes only if the "assert" does not fail.
-
-
-# assert " " $LINENO
-
-var=''
-# assert $var $LINENO
-
-# Some commands.
-# Some more commands . . .
-
-# . . .
-# More commands . . .
-
-exit $?
+condition="$a -lt $b"
+assert "$condition"

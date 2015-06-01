4f92a1749581005fbbe54758ebba6477d0d80484
Fredrik Svard <fredrik.svard@gmail.com>
2015-06-01 16:19:38 +0200

    added doctest

---
+++ packt_book2/src/stock_alerter/tests/test_doctest.py
@@ -0,0 +1,19 @@
+import doctest
+from datetime import datetime
+
+from ..stock import Stock
+
+
+def setup_stock_doctest(doctest):
+	s = stock.Stock("GOOG")
+	doctest.globs.update({"stock":s})
+
+
+def load_tests(loader, tests, pattern):
+	tests.addTests(doctest.DocTestSuite(stock, globs = {
+		"datetime":datetime,
+		"Stock": stock.Stock
+		}, setUp=setup_stock_doctest))
+	options = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+	tests.addTests(doctest.DocFileSuite("readme.txt", package="stock_alerter", opinionflags = opinions))
+	return tests

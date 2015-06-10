46d646f3f0025df59dec47f19508496af6a0f37e
Fredrik Svard <fredrik.svard@gmail.com>
2015-06-10 16:58:31 +0200

    Some new features

--- BDD/wiki/steps/wikipedia_search.py
+++ BDD/wiki/steps/wikipedia_search.py
@@ -1,5 +1,7 @@
 from behave import *
-import html5lib
+from hamcrest import *
+from html.parser import HTMLParser
+import re
 use_step_matcher("re")
 
 # http://jenisys.github.io/behave.example/tutorials/tutorial04.html
@@ -9,6 +11,14 @@ use_step_matcher("re")
 #
 #
 #
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
 
 @given('I can access Wikipedia')
 def step_impl(context):
@@ -22,6 +32,8 @@ def step_impl(context, text):
 @then(u'I get a result for "(?P<text>.*)"')
 def step_impl(context, text):
     assert text in context.browser.page_source
+    parser = MyHTMLParser()
+    print(parser.feed(context.browser.page_source))
 
 #@when(u'I search for (?P<text>.*)')
 @when(u'I search for (?P<text>[\w]+)')
@@ -45,7 +57,9 @@ def step_impl(context, text):
 def step_impl(context):
 #   expect(find('h1').text).to eq(@subject)
     assert context.subject in context.browser.page_source
-    pass
+    assert context.subject
+    assert context.browser.page_source
+    # validate_in_h1(context.browser.page_source, context.subject)
 
 @then(u'I find relevant information')
 def step_impl(context):
@@ -60,4 +74,31 @@ def validate_relevance_for(context,subject):
     assert subject in context.browser.page_source,"{}".format(subject)
     #assert 'is the largest rodent in the world' in context.browser.page_source
 
+def assertFalse():
+    assert False
+
+def new_validate_relevance_for(context,subject):
+    assert isinstance(subject, str), "{}".format(type(subject))
+    subject.replace('"','')
+
+    check = {"Capybara": "is the largest rodent in the world", 
+              "Selenium": "reduce the effects of mercury toxicity"}
+
+    result = check.get(subject,"Fredrik Svard")
+
+    assert subject in context.browser.page_source,"{}".format(subject)
+    assert result in context.browser.page_source, "{} {}".format(result, subject)
+
+def validate_in_h1(page, subject):
+
+    match = re.findall(r'<h1>.*</h1>', page)
+
+    assert match
+
+    for i in match:
+        print(i)
+        if subject == i:
+            return
+
+    assert False
 

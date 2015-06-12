64681be87d635894575df4e0eb1ed69b335d41a7
Fredrik Svard <fredrik.svard@gmail.com>
2015-06-12 16:14:02 +0200

    datetime stuff

---
+++ datetime/to_posix.py
@@ -0,0 +1,20 @@
+
+# - 150502161930  
+#   1284101485
+import datetime
+first = datetime.datetime.fromtimestamp(int("1284101485"))
+second = first.strftime('%Y-%m-%dT%H:%M:%SZ')
+print(second)
+print(first.isoformat())
+#print(datetime.datetime.fromtimestamp("150502161930").strftime('%Y-%m-%dT%H:%M:%SZ'))
+
+from dateutil.parser import *
+
+hwlog_ts = "150502161930"
+
+if not hwlog_ts.startswith("20"):
+    hwlog_ts = "20" + hwlog_ts
+
+nisse = parse(hwlog_ts)
+today = nisse.strftime('%Y-%m-%dT%H:%M:%SZ')
+print(today)

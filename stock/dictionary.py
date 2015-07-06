54b41c4bfdf55f854be248612cd0547c7eecf1aa
Fredrik Svard <Fredrik Svard@localhost>
2015-07-06 19:43:42 +0200

    Try write some BDD for some ReST features

--- stock/dictionary.py
+++ stock/dictionary.py
@@ -3,7 +3,8 @@
 dict = {'didner': 'http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P0000IWH7&programid=0000000000c',
        'lannebo': 'http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000F91&programid=0000000000',
 #       'baring': 'http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000RLK&programid=0000000000'
-        'kontiki': 'http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00008WHN&programid=0000000000'
+        'kontiki': 'http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00008WHN&programid=0000000000',
+        'sppusa': 'http://www.morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000FYR&programid=0000000000'
        }
 
 

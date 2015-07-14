525291ba8dd1e7aa0b43bb56bd76c7f6d2c858af
Fredrik Svard <fredrik.svard@gmail.com>
2015-07-14 12:15:53 +0200

    Some updating

--- series/read_cfiles.py
+++ series/read_cfiles.py
@@ -3,10 +3,13 @@ import os
 
 all_cfiles = [file for file in os.listdir('.') if file.endswith('.c')]
 
+used = list()
+
 for file in all_cfiles:
     with open(file, 'r') as fh:
         tot = fh.readlines()
-        #print(tot)
-        for row in tot:
+        for i, row in enumerate(tot):
             if "AG6" in row:
-                print(row) 
+                print(row.strip(), end=' ')
+                if '"' in tot[i+1]: 
+                    print(tot[i+1].strip())

62267d86e139976a7f9bd134d2be56b2a85ec33b
Fredrik Svard <fredrik.svard@gmail.com>
2015-07-16 14:06:46 +0200

    Some corrections

--- BDD/ReST/steps/namedhttpstatus.py
+++ BDD/ReST/steps/namedhttpstatus.py
@@ -7,9 +7,12 @@ class NamedHTTPstatus(object):
        "OK": 200,
        "CREATED": 201,
        "NO_CONTENT": 204,
+       "NOT_MODIFIED": 304,
        "BAD_REQUEST": 400,
        "UNAUTHORIZED": 401,
+       "FORBIDDEB": 403,
        "METHOD_NOT_ALLOWED": 405,
+       "CONFLICT" : 409,
        "INTERNAL_SERVER_ERROR": 500,        
     }
 

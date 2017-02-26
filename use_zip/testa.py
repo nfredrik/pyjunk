9e1243b30837532e64dcaab275a41654e22305d7
Fredrik Svard <fredrik.svard@gmail.com>
2017-02-26 12:39:58 +0100

    more

--- use_zip/testa.py
+++ use_zip/testa.py
@@ -8,29 +8,32 @@ from os.path import join
 def extract_files(zipf = "./volvo.zip", destdir ='./nuda'):
 
   if not os.path.isfile(zipf):
-    raise Exception("Error filen finns inte")
-
+    raise Exception("Error: no zipfile:" + zipf)
+  
   with zipfile.ZipFile(zipf) as myzip:
-    nille = myzip.extractall(destdir)
+    if myzip.testzip() != None:
+      raise Exception("Error: invalid zipfile:" + myzip)
+    
+    myzip.extractall(destdir)
+
 
   root = destdir + '/tests'  
+
   if not os.path.isdir(root):
-    raise Exception("Error mappen finns inte")
+    raise Exception("Error: not directory:" + root)
 
   return [join(root,f) for root,dirs,files in walk(root) for f in files]  
-    
+
+
 def main():
 
-  # Verify that we have a zipfile
-  # Extract it to defined directory
-  # Verify that we have a tests directory
-  # with files ...
-  # return array of files  
   the_dir = 'mydir'  
-  the_files = extract_files(destdir = the_dir)
+  the_files = extract_files(zipf = "./volvo.zip", destdir = the_dir)
+
   for file in the_files:
-    print file
-  
+    print "opening:" + file
+    with open(file) as fh:
+      print fh.read()
     
 
 if __name__ == '__main__':

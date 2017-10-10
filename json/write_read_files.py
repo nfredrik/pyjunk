3e2786442891fd7b9d6f972708fb885cfaa398ca
Fredrik Svard <fredrik.svard@gmail.com>
2017-10-10 22:45:29 +0200

    more on write json

---
+++ json/write_read_files.py
@@ -0,0 +1,24 @@
+import json
+
+data = {
+   'name' : 'ACME',
+   'shares' : 100,
+   'price' : 542.23
+}
+
+
+# Writing JSON data
+with open('data.json', 'w') as f:
+     json.dump(data, f)
+
+
+# Reading data back
+with open('data.json', 'r') as f:
+     data1 = json.load(f)
+
+data1['natti'] = 134     
+
+print(data1)   
+
+with open('data.json', 'w') as f:
+     json.dump(data1, f)

86add66b8668ee4a3c0f8c9cd5a860a9216abb15
Fredrik Svard <nfredrik@hotmail.com>
2026-05-24 16:59:30 +0200

    refactor

--- checktimestamp.py
+++ checktimestamp.py
@@ -1,27 +1,31 @@
 # Read a datetime string and check if it is older than 10 days
+import functools
 import time
 
 import datetime
 
 
-def check_this(the:datetime.datetime, name:str)-> bool:
-    if datetime.datetime.now() > the:
-        print(f"{name}:✅", end='')
+def check_this(last_tight:datetime.datetime, the:datetime.datetime, name:str)-> bool:
+    if last_tight < the:
+        print(f"{name} {the.isoformat(timespec='seconds')}:✅", end='')
 
         return True
-    print(f"{name}:❌", end='')
+    print(f"{name} {the.isoformat(timespec='seconds')}:❌", end='')
     return False
+
+latest_tightening = datetime.datetime.now()
 
 
 later1 = datetime.datetime.now() + datetime.timedelta(seconds=10)
 later2 = datetime.datetime.now() + datetime.timedelta(seconds=20)
 later3 = datetime.datetime.now() + datetime.timedelta(seconds=30)
 
+my_call = functools.partial(check_this, latest_tightening)
 while True:
 
 
     #if check_this(later1) and check_this(later2) and check_this(later3):
-    if all([check_this(later1, 'olle'),check_this(later2, 'dole'), check_this(later3, 'doff')]):
+    if all([my_call(later1, 'olle'),my_call(later2, 'dole'), my_call(later3, 'doff')]):
 
         break
 
